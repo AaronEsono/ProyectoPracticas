442971a4b887cc2c4b7e786d30677b43329ec0dd
ProyectoPracticas <aaron.esono576@gmail.com>
2024-04-11 16:32:17 +0200

    Proyecto Jueves 11/4 version 2

---
+++ app/src/main/java/com/example/practicaaaron/clases/pedidos/PedidoActualizar.kt
@@ -0,0 +1,10 @@
+package com.example.practicaaaron.clases.pedidos
+
+import com.google.gson.annotations.SerializedName
+
+data class PedidoActualizar(
+    @SerializedName("ID_PEDIDO")
+    var idPedido:Int,
+    @SerializedName("INCIDENCIA")
+    var incidencia:Int
+)
